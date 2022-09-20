2ea75e9fa981da48e26191a74911e32c67ad4336
Masoud Amjadi <masoudmanson@gmail.com>
2022-09-20 16:50:48 +0000

    Update 5 files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
-import "./index.css";
-
+import { CellBasic, CellHeader, TableHeader, TableRow, Table } from "czifui";
 import {
   ColumnDef,
   flexRender,
@@ -10,6 +8,7 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 import { makeData } from "./makeData";
+import "./index.css";
 
 type Person = {
   firstName: string;
@@ -22,63 +21,45 @@ type Person = {
 
 const defaultColumns: ColumnDef<Person>[] = [
   {
-    header: "Name",
+    accessorKey: "firstName",
+    cell: (info) => info.getValue(),
+    header: "First Name",
     footer: (props) => props.column.id,
-    columns: [
-      {
-        accessorKey: "firstName",
-        cell: (info) => info.getValue(),
-        footer: (props) => props.column.id,
-      },
-      {
-        accessorFn: (row) => row.lastName,
-        id: "lastName",
-        cell: (info) => info.getValue(),
-        header: () => <span>Last Name</span>,
-        footer: (props) => props.column.id,
-      },
-    ],
   },
   {
-    header: "Info",
+    accessorFn: (row) => row.lastName,
+    id: "lastName",
+    cell: (info) => info.getValue(),
+    header: "Last Name",
     footer: (props) => props.column.id,
-    columns: [
-      {
-        accessorKey: "age",
-        header: () => "Age",
-        footer: (props) => props.column.id,
-      },
-      {
-        header: "More Info",
-        columns: [
-          {
-            accessorKey: "visits",
-            header: () => <span>Visits</span>,
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: "status",
-            header: "Status",
-            footer: (props) => props.column.id,
-          },
-          {
-            accessorKey: "progress",
-            header: "Profile Progress",
-            footer: (props) => props.column.id,
-          },
-        ],
-      },
-    ],
+  },
+  {
+    accessorKey: "age",
+    header: "Age",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "visits",
+    header: "Visits",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    footer: (props) => props.column.id,
+  },
+  {
+    accessorKey: "progress",
+    header: "Profile Progress",
+    footer: (props) => props.column.id,
   },
 ];
 
 function App() {
-  const [data] = React.useState(() => makeData(1000));
+  const [data] = React.useState(() => makeData(100));
   const [columns] = React.useState<typeof defaultColumns>(() => [
     ...defaultColumns,
   ]);
-
-  const rerender = React.useReducer(() => ({}), {})[1];
 
   // Create the table and pass your options
   const table = useReactTable({
@@ -91,6 +72,10 @@ function App() {
   // Manage your own state
   const [state, setState] = React.useState(table.initialState);
 
+  React.useEffect(() => {
+    console.log(state);
+  }, [state]);
+
   // Override the state managers for the table to your own
   table.setOptions((prev) => ({
     ...prev,
@@ -99,125 +84,49 @@ function App() {
     // These are just table options, so if things
     // need to change based on your state, you can
     // derive them here
-
-    // Just for fun, let's debug everything if the pageIndex
-    // is greater than 2
-    debugTable: state.pagination.pageIndex > 2,
   }));
 
   return (
-    <div className="p-2">
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
+    <div className="app">
+      <h1 className="title">Fully Controlled Table</h1>
+      <p className="description">
+        Table that demonstrates how to access the table’s internal data
+        management state.
+      </p>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) =>
+            headerGroup.headers.map((header) => (
+              <CellHeader key={header.id} colSpan={header.colSpan} hideSortIcon>
+                {
+                  flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  ) as string
+                }
+              </CellHeader>
+            ))
+          )}
+        </TableHeader>
         <tbody>
           {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
+            <TableRow key={row.id}>
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
+                <CellBasic
+                  key={cell.id}
+                  primaryText={
+                    flexRender(
+                      cell.column.columnDef.cell,
+                      cell.getContext()
+                    ) as string
+                  }
+                  shouldShowTooltipOnHover={false}
+                />
               ))}
-            </tr>
+            </TableRow>
           ))}
         </tbody>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
-      </table>
-      <div className="h-2" />
-      <div className="flex items-center gap-2">
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(0)}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<<"}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          {"<"}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          {">"}
-        </button>
-        <button
-          className="border rounded p-1"
-          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-          disabled={!table.getCanNextPage()}
-        >
-          {">>"}
-        </button>
-        <span className="flex items-center gap-1">
-          <div>Page</div>
-          <strong>
-            {table.getState().pagination.pageIndex + 1} of{" "}
-            {table.getPageCount()}
-          </strong>
-        </span>
-        <span className="flex items-center gap-1">
-          | Go to page:
-          <input
-            type="number"
-            defaultValue={table.getState().pagination.pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              table.setPageIndex(page);
-            }}
-            className="border p-1 rounded w-16"
-          />
-        </span>
-        <select
-          value={table.getState().pagination.pageSize}
-          onChange={(e) => {
-            table.setPageSize(Number(e.target.value));
-          }}
-        >
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="h-4" />
-      <button onClick={() => rerender()} className="border p-2">
-        Rerender
-      </button>
+      </Table>
     </div>
   );
 }
